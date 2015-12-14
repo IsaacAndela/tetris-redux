ec6983da76bac0e73276f11a3a2fe6c2417b2df5
Isaac Andela <mail@isaacandela.nl>
2015-12-14 09:23:44 +0100

    new reducers for board

---
+++ reducers/newBoardReducer.js
@@ -0,0 +1,8 @@
+import Immutable from 'immutable';
+import {boardWidth, boardHeight} from 'gameSettings';
+
+const newRow = (columnCount) => Immutable.Repeat(0, columnCount).toList();
+
+const newRows = (rowCount, columnCount) => Immutable.Repeat(() => newRow(columnCount), rowCount).toList();
+
+export const newBoard = () => newRows(boardHeight, boardWidth);
